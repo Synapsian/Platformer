189a3aede6abdeaa66807ef160b3b2c03a9bb730
A Completely Normal Popcat <122589310+Synapsian@users.noreply.github.com>
2026-03-04 13:48:50 -0800

    Various changes
    Collision bugfix, level design change, color changes, spikes, player death mechanic

--- sketch.js
+++ sketch.js
@@ -13,41 +13,90 @@ function setup() {
   createCanvas(400, 400);
   // Level 0
   level = 0
-  new Static(100,300,100,level)
-  new Static(300,250,100,level)
-  new Static(300,100,20,level)
-  new Static(320,180,20,level)
+  new Static(0,450,[100,400],level)
+  new Static(75,510,[50,400],level)
+  new Static(150,450,[100,400],level)
+  new Static(225,510,[50,400],level)
+  new Static(300,450,[100,400],level)
+  new Static(400,510,[100,400],level)
+  new Static(500,450,[100,400],level)
+  new Static(600,510,[100,400],level)
+  new Static(700,450,[100,400],level)
   
-  new Goal(300,0,level)
+  new Spike(75,300,20,level)
+  new Spike(225,300,20,level)
+  new Spike(400,300,20,level)
+  new Spike(600,300,20,level)
+  
+  new Goal(700,150,level)
   
   // Level 1
   level = 1
-  new Static(100,300,100,level)
+  new Static(0,300,100,level)
   
-  LocalPlayer = new Player(100,100);
+  LocalPlayer = new Player(0,0);
 }
 
 class Static {
   constructor(x,y,size,level) {
     this.x = x
     this.y = y
-    this.size = size
+    
+    if (size instanceof Array) {
+      this.size = size
+    } else {
+      this.size = [size,size]
+    }
+    
     StaticObjects[level].push(this)
   }
   render() {
     fill('white')
     rectMode(CENTER)
-    rect(this.x,this.y,this.size)
+    rect(this.x,this.y,this.size[0],this.size[1])
   }
   checkCollision(x,y,size) {
-    const rightBound = this.x + this.size/2
-    const leftBound = this.x - this.size/2
-      
-    const upperBound = this.y + this.size/2
-    const lowerBound = this.y - this.size/2
+    const rightBound = this.x + this.size[0]/2
+    const leftBound = this.x - this.size[0]/2 
+    const upperBound = this.y + this.size[1]/2
+    const lowerBound = this.y - this.size[1]/2
+
       
     if (x + size/2 > leftBound && x - size/2 < rightBound && y - size/2 < upperBound && y + size/2 > lowerBound) {
       return true
+    }
+  }
+  collide(object) {
+    return
+  }
+}
+
+class Spike extends Static {
+  constructor(x,y,size,level) {
+    super(x,y,size,level)
+    this.hitboxSize = [this.size[0]/3,this.size[1]/3]
+  }
+  render() {
+    rectMode(CENTER)
+    fill('red')
+    triangle(this.x + this.size[0]/2,this.y + this.size[1]/2,this.x - this.size[0]/2, this.y + this.size[1]/2, this.x, this.y - this.size[1]/2)
+    //fill('lime')
+    //rect(this.x,this.y,this.hitboxSize[0],this.hitboxSize[1])
+  }
+  checkCollision(x,y,size) {
+    const rightBound = this.x + this.hitboxSize[0]/2
+    const leftBound = this.x - this.hitboxSize[0]/2
+    const upperBound = this.y + this.hitboxSize[1]/2
+    const lowerBound = this.y - this.hitboxSize[1]/2
+      
+    if (x + size/2 > leftBound && x - size/2 < rightBound && y - size/2 < upperBound && y + size/2 > lowerBound) {
+      return true
+    }
+  }
+  collide(object) {
+    if (object instanceof Player) {
+      object.die()
+      return
     }
   }
 }
@@ -59,13 +108,17 @@ class Goal extends Static {
   render() {
     fill('lime')
     rectMode(CENTER)
-    rect(this.x,this.y,this.size)
+    rect(this.x,this.y,this.size[0],this.size[1])
   }
-}
-
-class Platform extends Static {
-  constructor(x,y,sizeX,sizeY,level) {
-    
+  collide(object) {
+    if (object instanceof Player) {
+      Level++
+      object.x = 0
+      object.y = 0
+      object.velocity[0] = 0
+      object.velocity[1] = 0
+      return
+    }
   }
 }
 
@@ -73,6 +126,7 @@ class Player {
   constructor(x,y) {
     this.x = x
     this.y = y
+    this.speed = 5
     this.velocity = [0,0]
     this.coyoteTime = false
     
@@ -85,6 +139,7 @@ class Player {
   }
   render() {
     rectMode(CENTER)
+    fill('lightblue')
     rect(this.x,this.y,this.size)
   }
   tick() {
@@ -93,18 +148,24 @@ class Player {
     this.applyVelocity()
     this.render()
   }
-  applyVelocity() {
-    // todo: fix gliding when between two close objects 
-    // todo: fix clipping into the corners of objects
-    // X Check
-    if (this.getCollisions(this.x + this.velocity[0],this.y).length > 0) {
-      this.velocity[0] = 0
+  die() {
+    this.x = 0
+    this.y = 0
+    this.velocity[0] = 0
+    this.velocity[1] = 0
+  }
+  setState(type,duration) {
+    this.state.type = type
+    if (duration) {
+      this.state.duration = duration
+    } else {
+      this.state.duration = 0
     }
-    // Y Check
-    if (this.getCollisions(this.x,this.y + this.velocity[1]).length > 0) {
+  }
+  _verticalCollisions(collided) {
+    if (collided) {
       if (this.velocity[1] > 0) {
-        this.state.type = 'grounded'
-        this.state.duration = 10
+        this.setState('grounded',10)
       }
       this.velocity[1] = 0
     } else {
@@ -118,26 +179,55 @@ class Player {
         this.state.type = 'air'
       }
     }
+  }
+  applyVelocity() {
+    // todo: fix gliding when between two close objects 
+    
+    const collidedX = (this.getCollisions(this.x + this.velocity[0],this.y).length > 0)
+    const collidedY = (this.getCollisions(this.x,this.y + this.velocity[1]).length > 0)
+    const collisionsXY = this.getCollisions(this.x + this.velocity[0],this.y + this.velocity[1])
+    const collidedXY = (collisionsXY.length > 0)
+    
+    this._verticalCollisions(collidedY)
+    
+    if (collidedX) {
+      this.velocity[0] = 0
+    }
+    
+    if (!collidedX && !collidedY & collidedXY) {
+      // Moving diagonally into platform
+      if (collisionsXY.length == 1) {
+        const platform = collisionsXY[0]
+        const diff = this.x - platform.x
+        
+        if (this.velocity[0] < diff) {
+          this.x = platform.x + platform.size[0]/2 + this.size/2 - this.speed
+        } else {
+          this.x = platform.x - platform.size[0]/2 - this.size/2 + this.speed
+        }
+        
+      this.velocity[0] = 0
+      this.velocity[1] = 0
+      } 
+    }
     
     this.x += this.velocity[0]
     this.y += this.velocity[1]
     
     if (this.y > height) { // this is temporary
-      this.y = -height
-      this.velocity[1] = 20
-      this.x = 100
+      this.die()
     }
     
   }
   move() {
-    if (keyIsDown(87)) {
+    if (keyIsDown(87) || keyIsDown(38)) {
       if (this.state.type == 'grounded') {
         this.state.duration = 0
         this.applyForce(0,-15) 
       }
     } // W
-    if (keyIsDown(65)) { this.velocity[0] = -5 } // A
-    else if (keyIsDown(68)) { this.velocity[0] = 5 } // D
+    if (keyIsDown(65) || keyIsDown(37)) { this.velocity[0] = -this.speed } // A
+    else if (keyIsDown(68) || keyIsDown(39)) { this.velocity[0] = this.speed } // D
     else { this.velocity[0] = 0 }
   }
   applyGravity() {
@@ -155,14 +245,8 @@ class Player {
     let collisions = []
     
     for (const object of StaticObjects[Level]) {
-      
       if (object.checkCollision(x,y,this.size)) {
-        if (object instanceof Goal) {
-          Level++
-          this.x = 100
-          this.y = 100
-          return []
-        }
+        object.collide(this)
         collisions.push(object)
       }
     }
@@ -173,15 +257,18 @@ class Player {
 function draw() {
   background(220);
   fill('white')
+
+  textSize(20)
+  text('Level ' + Level,10,30)
   
   Camera.x = width/2 - LocalPlayer.x
   Camera.y = height/2 - LocalPlayer.y
   translate(Camera.x,Camera.y)
   
+  //noStroke()
   if (LocalPlayer) {
     LocalPlayer.tick()
   }
-  
   for (const object of StaticObjects[Level]) {
     object.render()
   }
